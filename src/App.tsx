870465969e537b92ff699b16546c0d303b64cfc5
马图图 <matuto@qq.com>
2026-06-21 08:19:41 +0800

    第3轮评测 - 1、npm run start 以后希望桌面客户端直接打开到康复机器人系统首页,打包出来的客户端双。

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { HashRouter, Routes, Route } from 'react-router-dom'
 import { Layout } from 'antd'
 import AppLayout from './components/Layout'
 import Dashboard from './pages/Dashboard'
@@ -10,7 +10,7 @@ import Insurance from './pages/Insurance'
 
 export default function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Layout style={{ minHeight: '100vh' }}>
         <Routes>
           <Route path="/" element={<AppLayout />}>
@@ -23,6 +23,6 @@ export default function App() {
           </Route>
         </Routes>
       </Layout>
-    </BrowserRouter>
+    </HashRouter>
   )
 }
